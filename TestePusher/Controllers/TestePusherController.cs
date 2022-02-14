56711c3b270210fd1fa1969fb68c3900e2fe05bb
Fernando Romano Batista <fernando.batista@hbsis.com.br>
2022-02-14 10:42:11 -0300

    Add project files.

---
+++ TestePusher/Controllers/TestePusherController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PusherServer;
+using System.Threading.Tasks;
+
+namespace TestePusher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestePusherController : ControllerBase
+    {        
+        private const string Cluster = "your-cluster";
+        private const string ChannelName = "your-channel";
+        private const string EventName = "your-event";
+        private const string AppId = "your-app-id";
+        private const string AppKey = "your-app-key";
+        private const string AppSecret = "your-app-secret";
+
+        [HttpPost]
+        public async Task<ActionResult> HelloWorld()
+        {
+            var options = new PusherOptions
+            {
+                Cluster = Cluster,
+                Encrypted = true
+            };
+
+            var pusher = new Pusher(
+                  AppId,
+                  AppKey,
+                  AppSecret,
+                  options);
+
+            var result = await pusher.TriggerAsync(
+              ChannelName,
+              EventName,
+              new { message = "hello world" });
+
+            return Ok(result);
+        }
+    }
+}
---
+++ TestePusher/TesteListener.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PusherClient;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestePusher
+{
+    public class TesteListener : IHostedService, IDisposable
+    {
+        private const string Cluster = "your-cluster";
+        private const string ChannelName = "your-channel";
+        private const string EventName = "your-event";        
+        private const string AppKey = "your-app-key";
+
+        private readonly Pusher _pusher;
+        private readonly ILogger<TesteListener> _logger;
+
+        public TesteListener(ILogger<TesteListener> logger)
+        {
+            _pusher = new Pusher(AppKey, new PusherOptions
+            {
+                Cluster = Cluster
+            });
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _pusher.ConnectAsync();
+            await _pusher.SubscribeAsync(ChannelName);
+            _pusher.Bind(EventName, (PusherEvent d) =>
+            {
+                Execute(d.Data);
+            });
+            _logger.LogInformation("Subscribed to channel.");
+        }
+
+        private void Execute(string data)
+        {
+            _logger.LogInformation(data);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _pusher.UnsubscribeAllAsync();
+            await _pusher.DisconnectAsync();
+        }
+
+        public async void Dispose()
+        {
+            await _pusher.UnsubscribeAllAsync();
+            await _pusher.DisconnectAsync();
+        }
+    }
+}
